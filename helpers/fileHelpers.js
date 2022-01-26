6d44f165b75fd74d0e4db90d76c4fc3bdffcb81f
J. Michael Brown <jmichaelbrown8@gmail.com>
2022-01-25 23:21:14 -0800

    Added endpoints for GET /api/notes, POST /api/notes, and DELETE /api/notes/:id, plus functionality with helpers.

---
+++ helpers/fileHelpers.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const util = require('util');
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+const writeToFile = (dest, data) => {
+    fs.writeFile(
+        dest, 
+        JSON.stringify(data), 
+        err => err ? console.error(err) : console.log(`Wrote to file: ${dest}`)
+    );
+};
+
+const readAndAppend = (filename, obj) => {
+    fs.readFile(filename, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+          } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(obj);
+            writeToFile(filename, parsedData);
+          }
+    })
+};
+
+module.exports = {
+    readAndAppend,
+    readFromFile,
+    writeToFile
+};
--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
+const { readAndAppend, readFromFile, writeToFile } = require('./helpers/fileHelpers');
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));  
 });
+
+app.get('/api/notes', (req, res) => {
+  // this is needed to immediately read from file
+  readFromFile('./db/db.json')
+    .then(
+      (data) => res.json(JSON.parse(data))
+    );
+});
+
+app.post('/api/notes', (req, res) => {
+  const note = { 
+    id: uuid(),
+    ...req.body 
+  };
+  readAndAppend( './db/db.json', note );
+  res.json( note );
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  readFromFile('./db/db.json')
+    .then( data => JSON.parse(data) )
+    .then( notesArray => notesArray.filter( note => note.id !== id ) )
+    .then( async newNotesArray => {
+      await writeToFile('./db/db.json', newNotesArray);
+      res.json( newNotesArray );
+    } )
+    ;
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html')) 
